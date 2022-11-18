f77a9744c84ea545b9d382ecda000178dcdb470c
andurb <sales.andurb@gmail.com>
2022-11-18 12:52:54 +0530

    Store openweather api key in gradle file (temporary fix)

--- app/src/main/java/com/deepakbarad/weatherapp/framework/data/datasources/remote/OpenWeatherRemoteDataSource.kt
+++ app/src/main/java/com/deepakbarad/weatherapp/framework/data/datasources/remote/OpenWeatherRemoteDataSource.kt
@@ -1,5 +1,6 @@
 package com.deepakbarad.weatherapp.framework.data.datasources.remote
 
+import com.deepakbarad.weatherapp.BuildConfig.OPEN_WEATHER_API_KEY
 import com.deepakbarad.weatherapp.framework.data.interfaces.IOpenWeatherDataSource
 import com.deepakbarad.weatherapp.framework.di.NetworkModule
 import com.deepakbarad.weatherapp.framework.model.CurrentWeather
@@ -14,7 +15,7 @@ class OpenWeatherRemoteDataSource @Inject constructor(
         val queryMap: MutableMap<String, String> = mutableMapOf()
         queryMap["lat"] = latitude.toString()
         queryMap["lon"] = longitude.toString()
-        queryMap["appid"] = "d242ceb4ca60d98d2385dc641f549b22"
+        queryMap["appid"] = OPEN_WEATHER_API_KEY
         val currentWeather = openWeatherApi.getForecast5(queryMap)
         println(currentWeather.body()?.message)
         return currentWeather.body() ?: throw Exception("No current weather info available")
