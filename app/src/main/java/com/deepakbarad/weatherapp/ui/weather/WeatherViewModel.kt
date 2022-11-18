735efd1616816eabaebe9645963a4e7e3acce5ae
andurb <sales.andurb@gmail.com>
2022-11-18 12:19:27 +0530

    Use String resource to display progress message

--- app/src/main/java/com/deepakbarad/weatherapp/ui/weather/WeatherViewModel.kt
+++ app/src/main/java/com/deepakbarad/weatherapp/ui/weather/WeatherViewModel.kt
@@ -1,13 +1,18 @@
 package com.deepakbarad.weatherapp.ui.weather
 
+import android.app.Application
+import android.content.Context
 import androidx.databinding.ObservableField
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.deepakbarad.weatherapp.R
 import com.deepakbarad.weatherapp.data.repository.OpenWeatherRepository
 import com.deepakbarad.weatherapp.model.CurrentWeather
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,8 +20,10 @@ import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class WeatherViewModel @Inject constructor(private val openWeatherRepository: OpenWeatherRepository) :
-    ViewModel() {
+class WeatherViewModel @Inject constructor(
+    private val context: Application,
+    private val openWeatherRepository: OpenWeatherRepository
+) : ViewModel() {
 
     val loadingFlag = ObservableField<Boolean>()
     val loadingMessage = ObservableField<String>()
@@ -30,7 +37,7 @@ class WeatherViewModel @Inject constructor(private val openWeatherRepository: Op
     val currentWeather: LiveData<CurrentWeather> get() = mCurrentWeather
 
     fun getForecast5(longitude: Double, latitude: Double) {
-        loadingMessage.set("Fetching weather forecast...")
+        loadingMessage.set(context.getString(R.string.fetching_weather_forecast))
         loadingFlag.set(true)
         viewModelScope.launch(Dispatchers.IO + exceptionHandler) {
             mCurrentWeather.postValue(openWeatherRepository.getForecast5(longitude, latitude))
